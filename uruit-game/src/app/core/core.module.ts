dd5bf03daf5042bb24e81fa1ca73f224f38c72ff
Jeremias Pais <jeremias.pais@synapse.com>
2018-04-27 11:09:15 -0300

    Header with routes

--- uruit-game/src/app/core/core.module.ts
+++ uruit-game/src/app/core/core.module.ts
@@ -3,19 +3,23 @@ import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { StatsComponent } from './stats/stats.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
- declarations: [
+  imports: [
+    RouterModule
+  ],
+  declarations: [
    HomeComponent,
    FooterComponent,
    HeaderComponent,
    StatsComponent
- ],
- exports: [
-  HomeComponent,
-  FooterComponent,
-  HeaderComponent,
-  StatsComponent
-]
+  ],
+  exports: [
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    StatsComponent
+  ]
 })
 export class CoreModule {}
